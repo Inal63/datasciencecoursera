a1058b3eff88e096e585d854fa0666674a9abdbd
Inal63 <natera.i63@gmail.com>
2014-05-17 13:28:15 -0400

    summary different notes in R programming class
    from vectors to control structures

---
+++ notes week1 R jh 3a.R
@@ -0,0 +1,7 @@
+vector() --creates an empty vector
+c() --groups things together called concatenate function
+Inf
+NaN
+1L
+
+--
---
+++ pollutant_dummy.R
@@ -0,0 +1,11 @@
+##write a function named 'pollutantmean' that claculates the mean of a pollutant (sulfate or nitrate) across a specified list of monitors.
+
+pollutantmean <- function (directory, pollutant, id = 1:332) {##Where something is going to happen
+  directory <- c(length = 1,"~/datasciencecoursera/specdata") ##that's has to be part of outcome 'specdata'
+  pollutant <- c("sulfate", "nitrate") ##not sure what goes in here either yet") ## where sulfate or nitrate are calculated
+    mean(id("pollutant", rm.na = TRUE)/id
+    
+    date <- formatted yyyy-mm-dd
+
+
+}
